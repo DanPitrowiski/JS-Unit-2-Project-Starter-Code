e7a6606726243eff9e7e961c2b788b8813c9d35c
Dan Pitrowiski <danpitrowiski@gmail.com>
2017-10-30 18:10:20 -0700

    Everything is working at least right now

--- js/app.js
+++ js/app.js
@@ -7,16 +7,27 @@
 // Just get an API to work
 'use strict';
 
+let globalIndex = 0;
+
 let digg = {
 	url: 'https://accesscontrolalloworiginall.herokuapp.com/http://digg.com/api/news/popular.json',
 	source: 'digg',
-}
+};
 
-var apiLinkGuardian = 'https://content.guardianapis.com/search?api-key=';
-var apiKeyGuardian = 'db045aa3-693a-4609-a87e-e895500f7a80';
+let hackernews = {
+	url: 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty',
+	source: 'hn',
+};
 
+let guardiannews = {
+	api: 'https://content.guardianapis.com/search?api-key=',
+	key: 'db045aa3-693a-4609-a87e-e895500f7a80',
+	url: this.api+this.key,
+	source: 'guardian',
+};
 
-function postStories(url, source){
+//DIGG
+function postDigg(url, source){
 	// debugger;
 	$.get(url, function(){
 		console.log("succeded");
@@ -32,28 +43,88 @@ function postStories(url, source){
 			$("#main").append(template);
 			//Adding an event listener to the new content
 			addEventListener(content, index);
+			globalIndex++;
+		})
+	})
+}
+
+// The Guardian
+function postGuardian(url, source){
+	// debugger;
+	api.editions.search('us') //make the call
+	.then(function(response){
+    	console.log(response.body); //do something with the response
+  	})
+  	.catch(function(err){
+    	console.log(err);
+  	});
+}
+
+// Hackernews
+function postHackerNews(url, source){
+	// debugger;
+	$.get(url, function(){
+		console.log("succeded");
+	})
+	.done(function(result){
+		console.log("Hackernews:");
+		result.slice(0,10).forEach(function(element, index){
+			$.get("https://hacker-news.firebaseio.com/v0/item/"+result[index]+".json?print=pretty", function(){
+	  		console.log("succeded");
+			})
+			.done(function(result){
+				//HN content
+				console.log(result);
+				let content = createContent(result, element, source);
+				console.log(content);
+				// Creating a template
+				let template = templateHTML(content, index);
+				$("#main").append(template);
+				//Adding an event listener to the new content
+				addEventListener(content, index);
+				globalIndex++;
+				console.log("Element: " + element);
+				console.log("Index: " + index);
+			})
 		})
 	})
 }
 
 function createContent(result, element, source){
-	console.log("Result")
+
 	if (source === "digg"){
-		let content = {
+		var articleContent = {
 		  title: element.content.title,
 		  description: element.content.description,
 		  score: element.diggs.count,
 		  image: element.content.media.images[0].url,
 		  tag: element.content.tags["0"].display_name,
 		  url: element.content.url,
-		}
+		};
+		return articleContent;
 	}
-	return content;
+	if (source === "hn"){
+		let tagname;
+		if (typeof result.type === "undefined"){
+			tagname = "hackernews";
+		}else{
+			tagname = result.type;
+		}
+		var articleContent = {
+			title: result.title,
+			description: "",
+		  	score: result.score,
+			image: "images/hackernews-logo.jpg",
+			tag: tagname,
+			url: result.url,
+		};
+		return articleContent;
+	}
 }
 
 function templateHTML(content, index){
 	    let template = `
-	  <article id="article-${index}" class="article">
+	  <article id="article-${globalIndex}" class="article">
 	    <section class="featuredImage">
 	      <img src="${content.image}" alt="" />
 	    </section>
@@ -71,12 +142,11 @@ function templateHTML(content, index){
 }
 
 function addEventListener(content, index){
-		$(`#article-${index}`).click(function(){
-			$('#popUp').removeClass('loader hidden')
-			$('#popUp h1').text(content.title);
-			$('#popUp p').text(content.description);
-
-			$('#popUp a.popUpAction').attr("href" , content.url);
+	$(`#article-${globalIndex}`).click(function(){
+		$('#popUp').removeClass('loader hidden')
+		$('#popUp h1').text(content.title);
+		$('#popUp p').text(content.description);
+		$('#popUp a.popUpAction').attr("href" , content.url);
 
 		$('#popUp .closePopUp').click(function(){
 			$('#popUp').addClass('hidden')
@@ -84,4 +154,49 @@ function addEventListener(content, index){
 	})
 }
 
-postStories(digg.url, digg.source);
+ $( document ).ready(function() {
+ 	loadingOn();
+	postDigg(digg.url, digg.source);
+	postHackerNews(hackernews.url, hackernews.source);
+	// postGuardian(guardiannews.url, guardiannews.source);
+	loadingOff();
+ });
+
+// PAGE INTERACTIONS
+
+$('.digg').click( function(){
+	loadingOn();
+	$( "#main" ).children().remove();
+	postDigg(digg.url, digg.source);
+	$(".source").html("Digg")
+	loadingOff();
+});
+
+$('.hackernews').click( function(){
+	loadingOn();
+	$( "#main" ).children().remove();
+	postHackerNews(hackernews.url, hackernews.source);
+	$(".source").html("Hackernews");
+	loadingOff();
+});
+
+$('#feedr').click( function(){
+	loadingOn();
+	$( "#main" ).children().remove();
+	postDigg(digg.url, digg.source);
+	postHackerNews(hackernews.url, hackernews.source);
+	loadingOff();
+});
+
+$('.search-icon').click( function(){
+	$("#search").toggleClass("active");
+});
+
+function loadingOn(){
+	$('#popUp').addClass('loader');
+	$('#popUp').removeClass('hidden');
+}
+function loadingOff(){
+	$('#popUp').removeClass('loader');
+	$('#popUp').addClass('hidden');
+}
